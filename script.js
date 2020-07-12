a97c5003185f4906b79fe11f75f5c964c3d874ec
Tyler Mathews <mrmathews08@gmail.com>
2020-07-12 16:18:36 -0600

    put into textbox

--- script.js
+++ script.js
@@ -12,15 +12,17 @@ number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"];
 space = [];
 
+var choices;
+
 //Uppercase conversion for letters
 var toUpper = function (x) {
     return x.toUpperCase();
 };
- var choices;
+
 //created a var for conversion
 alpha2 = alpha.map(toUpper);
 
-var start =document.querySelector("generate");
+var start = document.querySelector(".btn");
 
 start.addEventListener("click", function () {
     ps = generatePassword();
@@ -30,7 +32,7 @@ start.addEventListener("click", function () {
 //Function for password generator
 
 function generatePassword() {
-    enter = parseInt(prompt)("How many characters?");
+    enter = parseInt(prompt("How many characters?"));
     if (!enter) {
         alert("Please input a value");
     } else if (enter < 8 || enter > 128) {
@@ -41,7 +43,7 @@ function generatePassword() {
         confirmCharacter = confirm("Will this password include special characters?");
         confirmUppercase = confirm("Will this password contain Uppercase letters?");
         confirmLowercase = confirm("will this password include Lowercase letters?");
-    };
+};
 if (!confirmCharacter && !confirmNumber && !confirmUppercase && !confirmLowercase) {
     choices = alert("You must enter a value!");
 
@@ -58,6 +60,49 @@ if (!confirmCharacter && !confirmNumber && !confirmUppercase && !confirmLowercas
  else if (confirmNumber && confirmLowercase && confirmUppercase) {
      choices = number.concat(alpha, alpha2);
  }
+ else if (confirmCharacter && confirmNumber) {
+    choices = character.concat(number);
+
+} else if (confirmCharacter && confirmLowercase) {
+    choices = character.concat(alpha);
+
+} else if (confirmCharacter && confirmUppercase) {
+    choices = character.concat(alpha2);
+}
+else if (confirmLowercase && confirmNumber) {
+    choices = alpha.concat(number);
+
+} else if (confirmLowercase && confirmUppercase) {
+    choices = alpha.concat(alpha2);
+
+} else if (confirmNumber && confirmUppercase) {
+    choices = number.concat(alpha2);
+}
+else if (confirmCharacter) {
+    choices = character;
+}
+else if (confirmNumber) {
+    choices = number;
+}
+else if (confirmLowercase) {
+    choices = alpha;
+}
+else if (confirmUppercase) {
+    choices = space.concat(alpha2);
+};
+
+var password = [];
+
+
+for (var i = 0; i < enter; i++) {
+    var userChoices = choices[Math.floor(Math.random() * choices.length)];
+    password.push(userChoices);
+}
+
+var ps = password.join("");
+UserInput(ps);
+return ps;
+
 }
 
 
